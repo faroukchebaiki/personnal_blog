f68d9747e31f2a6881ecd07310e139905783b650
Farouk Chebaiki <f7484r12@gmail.com>
2022-11-26 22:53:55 +0100

    adding some navigation

--- src/Components/Top.js
+++ src/Components/Top.js
@@ -1,8 +1,17 @@
 export default function Top() {
     return (
-        <div className="">
-
-
-        </div>
+        <header className="">
+            <nav className="nav">
+                <a href='#A'><h2 className="">Logo</h2></a>
+                <div>
+                    <ul className="">
+                        <li><a href="#A">Home</a></li>
+                        <li><a href="#b">Main</a></li>
+                        <li><a href="#c">About</a></li>
+                    </ul>
+                </div>
+            </nav>
+            <div className=""></div>
+        </header>
     );
 }
