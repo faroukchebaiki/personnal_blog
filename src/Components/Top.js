32513f1bc6461314569d98e1eee8de9bd782a9c7
Farouk Chebaiki <f7484r12@gmail.com>
2022-11-27 03:23:20 +0000

    adding some animation 1

--- src/Components/Top.js
+++ src/Components/Top.js
@@ -10,15 +10,26 @@ export default function Top() {
         <header className="">
             <nav className="nav ontop">
                 <a href='#A'><h3 className="">Logo</h3></a>
-                <div>
-                    <ul className="buttons">
+                <div >
+                    <ul className="buttons ${className}">
                         <li><a href="#A">Home</a></li>
                         <li><a href="#b">Main</a></li>
                         <li><a href="#c">About</a></li>
                     </ul>
                 </div>
                 <div className="burger">
-                    <Hamburger toggled={isOpen} toggle={setOpen} />
+                    <Hamburger 
+                        toggled={isOpen} 
+                        toggle={setOpen}
+                        onToggle={toggled=> {
+                            let className = 'buttons';
+                            if (toggled) {
+                            className += ' slideIn';
+                            } else {
+                            // close a menu
+                            }
+                            }}
+                    />
                 </div>
             </nav>
             <div className="nav onbottom"></div>
