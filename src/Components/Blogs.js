a57fb73339b077d3c02a6213411a37224daf95b6
Farouk Chebaiki <f7484r12@gmail.com>
2022-11-28 20:23:47 +0100

    adding posts

--- src/Components/Blogs.js
+++ src/Components/Blogs.js
@@ -1,9 +1,8 @@
-
+import Post from './Post';
 export default function Blogs() {
     return (
         <div className="right">
-            
-
+            <Post />
         </div>
     );
 }
---
+++ src/Components/Post.js
@@ -0,0 +1,14 @@
+export default function post() {
+    return (
+        <div className="post">
+            <div className="ontop">
+                <h1>Blog</h1>
+                <p>Blog content</p>
+            </div>
+            <div className="onbottom">
+
+            </div>
+        </div>
+
+    );
+}
