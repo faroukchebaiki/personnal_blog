aea02c64d930c617ddf1d194c2ded0902ecd1884
Farouk Chebaiki <f7484r12@gmail.com>
2022-11-28 03:10:30 +0000

    adding the main sections

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
 
     return (
-        <footer className="main">
+        <footer className="footer">
             
 
         </footer>
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,8 +1,10 @@
+import Aside from "./Aside";
+import Blogs from "./Blogs";
 export default function Main() {
     return (
         <section className="main">
-
-
+            <Aside />
+            <Blogs />
         </section>
     );
 }
