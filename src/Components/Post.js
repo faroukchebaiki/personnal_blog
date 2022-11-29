f0571bd81566a38d445112afd3f13d50de07d470
Farouk Chebaiki <farouk.chebaiki@gmail.com>
2022-11-29 04:24:23 +0100

    adding content to post

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -2,8 +2,12 @@ export default function post() {
     return (
         <div className='post bottompost'>
             <div className="post toppost">
-                <h1>Blog</h1>
-                <p>Blog content</p>
+                <div className="posttext" >
+                    <h6>September 5, 2008</h6>
+                    <h1>My First Post</h1>
+                    <p>sqaoko okoqaok iojiqaj ppoazjs poajpojap poja jsp sos ajpijapap ppjpa opaoja et ojeppppao eoja pojapoj pojapoajd opaoja et ojeppppao eoja pojapoj pojapoajd popaoja et ojeppppao eoja pojapoj pojapoajd ^pqslm KJIEDE MLN </p>
+                    <a href='#read'>Read More</a>
+                </div>
             </div>
 
         </div>
