12ee2f8d793cf3e82da478da796ed7dc41c0acad
Farouk Chebaiki <farouk.chebaiki@gmail.com>
2022-12-01 01:00:34 +0000

    fixing the layout of aside and foooter

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,11 @@
+import Writer from "./Writer";
+import Copyright from "./Copyright";
 export default function Footer() {
 
     return (
         <footer className="footer">
-            
-
+            <Writer />
+            <Copyright />
         </footer>
     );
 }
--- src/Components/Writer.js
+++ src/Components/Writer.js
@@ -1,10 +1,11 @@
 export default function Writer() {
 
     return (
-        <div className="Writer">
+        <div className="writer">
             <div className="profile"></div>
+            <h4>John doe</h4>
             <h6>Writer</h6>
-            <p>Write your own stories and share them with the world.</p>
+            <p>Write your own stories and share them with the world. Write your own stories and share them with the world.Write your own stories and share them with the world.Write your own stories and share them with the world.</p>
         </div>
     );
 }
