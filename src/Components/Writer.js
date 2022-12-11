be08b2ff2cbed82d12c6c3fbb0b681357581ca66
Farouk Chebaiki <f7484r12@gmail.com>
2022-12-11 21:53:47 +0100

    improving the main section

--- src/Components/Writer.js
+++ src/Components/Writer.js
@@ -1,8 +1,11 @@
+import writer from './writer.png';
 export default function Writer() {
 
     return (
         <div className="writer">
-            <div className="profile"></div>
+            <div className="profile">
+                <img src={writer}  alt="logo" />
+            </div>
             <h4>John doe</h4>
             <h6>Writer</h6>
             <p>Write your own stories and share them with the world. Write your own stories and share them with the world.Write your own stories and share them with the world.Write your own stories and share them with the world.</p>
